327acefc1a1d81a8499520528eb71a8c06ea46ed
Michael Lai <mrlai7782@gmail.com>
2021-05-24 18:55:38 -0700

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App = () => {
-  return <div>App </div>;
+  return (
+    <div>
+      <Router>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
---
+++ client/src/routes/RestaurantdetailPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RestaurantdetailPage = () => {
+  return <div>Detail page</div>;
+};
+
+export default RestaurantdetailPage;
