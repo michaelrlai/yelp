b77313f1a99f838120bef199c57c121d1287b421
Michael Lai <mrlai7782@gmail.com>
2021-05-27 10:03:24 -0700

    update

--- client/src/routes/RestaurantdetailPage.jsx
+++ client/src/routes/RestaurantdetailPage.jsx
@@ -1,6 +1,8 @@
 import React, { useContext, useEffect } from 'react';
 import { useParams } from 'react-router';
 import RestaurantFinder from '../apis/RestaurantFinder';
+import Reviews from '../components/Reviews';
+import StarRating from '../components/StarRating';
 import { RestaurantContext } from '../context/RestaurantContext';
 
 const RestaurantdetailPage = () => {
@@ -19,7 +21,17 @@ const RestaurantdetailPage = () => {
     };
     fetchData();
   }, []);
-  return <div>{selectedRestaurant && selectedRestaurant.name}</div>;
+  return (
+    <div>
+      {selectedRestaurant && (
+        <>
+          <div className="mt-3">
+            <Reviews />
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default RestaurantdetailPage;
